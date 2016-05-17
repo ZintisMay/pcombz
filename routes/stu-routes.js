c6e5b5d63c2d14f144663493f460694a713d1d66
Zintis <Zintis>
2016-05-17 19:35:04 -0400

    a1

---
+++ routes/stu-routes.js
@@ -0,0 +1,36 @@
+// require path
+var path = require('path');
+
+// test if user is student
+function stuTest(req, inst) {
+	if(req.decoded.role == 'student') {
+		return true;
+	}
+	if (inst && req.decoded.role == 'instructor') {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+module.exports = function(app) {
+	app.get('/student', function(req, res){
+		if (stuTest(req, false)) {
+			res.sendFile(path.join(__dirname + '/../viewa/student.html'));
+		}
+		else{
+			res.sendFile(path.join(__dirname + '/../views/login.html'))
+		}
+	});
+
+	app.get('/comments/:id', function(req, res){
+		console.log(req.decoded);
+		if (stuTest(req, true)) {
+			res.sendFile(path.join(__dirname + '/../views/comments.html'));
+		}
+		else{
+			res.sendFile(path.join(__dirname + '/../views/login.html'));
+		}
+	});
+}
